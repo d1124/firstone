155d7a951b048406662578f26f93379a8a53f28d
d1124 <13501249800@163.com>
2021-03-19 21:53:54 +0800

    1

--- a/f.js
+++ a/f.js
@@ -1,7 +1,7 @@
 option = {
     title: {
-        text: '世界人口总量',
-        subtext: '数据来自网络'
+        text: '区域数据',
+        subtext: '数据对比'
     },
     tooltip: {
         trigger: 'axis',
@@ -10,7 +10,7 @@ option = {
         }
     },
     legend: {
-        data: ['2011年', '2012年']
+        data: ['近期平均数据', '实时数据']
     },
     grid: {
         left: '3%',
@@ -24,18 +24,18 @@ option = {
     },
     yAxis: {
         type: 'category',
-        data: ['巴西', '印尼', '美国', '印度', '中国', '世界人口(万)']
+        data: ['氧气', '瓦斯', 'CO2', 'N2', '其他气体', '正常气体含量']
     },
     series: [
         {
-            name: '2011年',
+            name: '近期平均数据',
             type: 'bar',
-            data: [18203, 23489, 29034, 104970, 131744, 630230]
+            data: [20, 0, 2, 77, 1, 100]
         },
         {
-            name: '2012年',
+            name: '实时数据',
             type: 'bar',
-            data: [19325, 23438, 31000, 121594, 134141, 681807]
+            data: [18, 3, 1, 77, 1, 99]
         }
     ]
 };
@@ -44,8 +44,8 @@ echarts.init(document.getElementById('t1'),'dark').setOption(option);
 
 option2 = {
     title: {
-        text: '未来一周气温变化',
-        subtext: '纯属虚构'
+        text: '温度变化',
+        subtext: '当天温度'
     },
     tooltip: {
         trigger: 'axis'
@@ -68,7 +68,7 @@ option2 = {
     xAxis: {
         type: 'category',
         boundaryGap: false,
-        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        data: ['0', '4', '7', '10', '13', '16', '20']
     },
     yAxis: {
         type: 'value',
@@ -78,9 +78,9 @@ option2 = {
     },
     series: [
         {
-            name: '最高气温',
+            name: '最高温度',
             type: 'line',
-            data: [10, 11, 13, 11, 12, 12, 9],
+            data: [10, 11, 13, 16, 12, 12, 9],
             markPoint: {
                 data: [
                     {type: 'max', name: '最大值'},
@@ -94,9 +94,9 @@ option2 = {
             }
         },
         {
-            name: '最低气温',
+            name: '最低温度',
             type: 'line',
-            data: [1, -2, 2, 5, 3, 2, 0],
+            data: [5, 3, 6, 9, 7, 6, 4],
             markPoint: {
                 data: [
                     {name: '周最低', value: -2, xAxis: 1, yAxis: -1.5}
@@ -128,13 +128,13 @@ echarts.init(document.getElementById('t2'),'dark').setOption(option2);
 
 option3 = {
     title: {
-        text: '折线图堆叠'
+        text: '气体浓度'
     },
     tooltip: {
         trigger: 'axis'
     },
     legend: {
-        data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+        data: ['N2', 'O2', 'CO2', '其他气体', '瓦斯']
     },
     grid: {
         left: '3%',
@@ -150,42 +150,133 @@ option3 = {
     xAxis: {
         type: 'category',
         boundaryGap: false,
-        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        data: ['0', '5', '10', '15', '20', '25', '30','35','40','45','50','55']
     },
     yAxis: {
         type: 'value'
     },
     series: [
         {
-            name: '邮件营销',
+            name: 'N2',
             type: 'line',
             stack: '总量',
-            data: [120, 132, 101, 134, 90, 230, 210]
+            data: [77, 78, 77, 78, 79, 77, 75]
         },
         {
-            name: '联盟广告',
+            name: 'O2',
             type: 'line',
             stack: '总量',
-            data: [220, 182, 191, 234, 290, 330, 310]
+            data: [18, 20, 19, 18,19, 20, 19]
         },
         {
-            name: '视频广告',
+            name: 'CO2*10',
             type: 'line',
             stack: '总量',
-            data: [150, 232, 201, 154, 190, 330, 410]
+            data: [10, 7, 9, 8, 7,10, 6]
         },
         {
-            name: '直接访问',
+            name: '其他气体*10',
             type: 'line',
             stack: '总量',
-            data: [320, 332, 301, 334, 390, 330, 320]
+            data: [12, 5, 6, 10, 12, 8, 6]
         },
         {
-            name: '搜索引擎',
+            name: '瓦斯*100',
             type: 'line',
             stack: '总量',
-            data: [820, 932, 901, 934, 1290, 1330, 1320]
+            data: [1,1, 1, 1, 4, 33, 76]
         }
     ]
 };
 echarts.init(document.getElementById('t3'),'dark').setOption(option3);
+
+option4 = {
+    tooltip: {
+        trigger: 'item'
+    },
+    legend: {
+        top: '5%',
+        left: 'center'
+    },
+    series: [
+        {
+            name: '访问来源',
+            type: 'pie',
+            radius: ['40%', '70%'],
+            avoidLabelOverlap: false,
+            label: {
+                show: false,
+                position: 'center'
+            },
+            emphasis: {
+                label: {
+                    show: true,
+                    fontSize: '40',
+                    fontWeight: 'bold'
+                }
+            },
+            labelLine: {
+                show: false
+            },
+            data: [
+                {value: 77, name: 'N2'},
+                {value: 20, name: 'O2'},
+                {value: 1, name: 'CO2*10'},
+                {value: 1, name: '瓦斯*100'},
+                {value: 1, name: '其他气体'}
+            ]
+        }
+    ]
+};
+echarts.init(document.getElementById('t4'),'dark').setOption(option4);
+
+
+function getVirtulData(year) {
+    year = year || '2017';
+    var date = +echarts.number.parseDate(year + '-01-01');
+    var end = +echarts.number.parseDate((+year + 1) + '-01-01');
+    var dayTime = 3600 * 24 * 1000;
+    var data = [];
+    for (var time = date; time < end; time += dayTime) {
+        data.push([
+            echarts.format.formatTime('yyyy-MM-dd', time),
+            Math.floor(Math.random() * 100)
+        ]);
+    }
+    return data;
+}
+
+option5 = {
+    title: {
+        top: 30,
+        left: 'center',
+        text: '每天的风险评级'
+    },
+    tooltip: {},
+    visualMap: {
+        min: 0,
+        max: 100,
+        type: 'piecewise',
+        orient: 'horizontal',
+        left: 'center',
+        top: 65
+    },
+    calendar: {
+        top: 120,
+        left: 30,
+        right: 30,
+        cellSize: ['auto', 13],
+        range: '2016',
+        itemStyle: {
+            borderWidth: 0.5
+        },
+        yearLabel: {show: false}
+    },
+    series: {
+        type: 'heatmap',
+        coordinateSystem: 'calendar',
+        data: getVirtulData(2016)
+    }
+};
+echarts.init(document.getElementById('t5'),'dark').setOption(option5);
+
